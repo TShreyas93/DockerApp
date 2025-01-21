9ac7e7eaefcf2cb72d93886e3282ef9a5a8e52e2
tshrey17 <shreyastandel093@gmail.com>
2025-01-21 18:00:47 +0530

    Initial commit for the new repository

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,18 +1,35 @@
 package com.example;
 
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.util.ssl.SslContextFactory;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        Server server = new Server(8080);
+        // Create a Jetty server
+        Server server = new Server();
 
+        // Configure SSL
+        SslContextFactory.Server sslContextFactory = new SslContextFactory.Server();
+        sslContextFactory.setKeyStorePath("/home/ec2-user/Website/Certs/keystore.jks"); // Path to the keystore
+        sslContextFactory.setKeyStorePassword("DummyPassword123"); // Keystore password
+        sslContextFactory.setKeyManagerPassword("DummyPassword123"); // Key manager password
+        sslContextFactory.setEndpointIdentificationAlgorithm(null);
+
+        // Create an HTTPS connector
+        ServerConnector sslConnector = new ServerConnector(server, sslContextFactory);
+        sslConnector.setPort(8443); // HTTPS port
+        server.addConnector(sslConnector);
+
+        // Configure the servlet context handler
         ServletContextHandler handler = new ServletContextHandler();
         handler.addServlet(new ServletHolder(new HelloServlet()), "/");
 
         server.setHandler(handler);
 
+        // Start the server
         server.start();
         server.join();
     }
